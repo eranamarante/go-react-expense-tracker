3385adf906f3d0b0f39ca6d9e28f5805b26fc9a8
Eran Amarante <eran.amarante@yahoo.com.ph>
2021-07-02 16:53:33 +0800

    update router

--- server/router/router.go
+++ server/router/router.go
@@ -1,18 +1,23 @@
 package router
 
-import "github.com/gorilla/mux"
+import (
+	"github.com/eranamarante/go-react-expense-tracker/server/middleware"
+	"github.com/gorilla/mux"
+)
 
 func Router() *mux.Router {
 	r := mux.NewRouter()
 
 	// Auth Endpoints
-	r.HandleFunc("api/auth/login", authMiddleware.Login).Methods("POST", "OPTIONS")
-	r.HandleFunc("api/auth/login", authMiddleware.Signup).Methods("POST", "OPTIONS")
+	// r.HandleFunc("api/auth/login", authMiddleware.Login).Methods("POST", "OPTIONS")
+	// r.HandleFunc("api/auth/login", authMiddleware.Signup).Methods("POST", "OPTIONS")
 	
 	// Expense Endpoints
-	r.HandleFunc("api/expenses", expenseMiddleware.GetAllExpenses).Methods("GET", "OPTIONS")
-	r.HandleFunc("api/expenses/{id}", expenseMiddleware.GetExpense).Methods("GET", "OPTIONS")
-	r.HandleFunc("api/expenses/new", expenseMiddleware.AddExpense).Methods("POST", "OPTIONS")
-	r.HandleFunc("api/expenses/{id}/edit", expenseMiddleware.UpdateExpense).Methods("PUT", "OPTIONS")
-	r.HandleFunc("api/expenses/{id}/delete", expenseMiddleware.DeleteExpense).Methods("DELETE", "OPTIONS")
+	r.HandleFunc("/api/expenses", middleware.GetAllExpenses).Methods("GET", "OPTIONS")
+	// r.HandleFunc("api/expenses/{id}", middleware.GetExpense).Methods("GET", "OPTIONS")
+	r.HandleFunc("/api/expenses/new", middleware.AddExpense).Methods("POST", "OPTIONS")
+	// r.HandleFunc("api/expenses/{id}/edit", middleware.UpdateExpense).Methods("PUT", "OPTIONS")
+	// r.HandleFunc("api/expenses/{id}/delete", middleware.DeleteExpense).Methods("DELETE", "OPTIONS")
+
+	return r
 }
