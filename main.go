c5a3569b551c04244ca3c9d312cd7281970c1b3c
Eran Amarante <eran.amarante31@gmail.com>
2021-07-12 16:08:37 +0800

    update main.go

--- main.go
+++ main.go
@@ -1,38 +1,20 @@
 package main
 
 import (
-	"os"
-
-	middleware "github.com/eranamarante/go-jwt-auth-api/middleware"
-	routes "github.com/eranamarante/go-jwt-auth-api/routes"
-
+	"github.com/eranamarante/go-expense-tracker-api/helper"
+	"github.com/eranamarante/go-expense-tracker-api/middleware"
+	"github.com/eranamarante/go-expense-tracker-api/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	port := os.Getenv("PORT")
-
-	if port == "" {
-		port = "8000"
-	}
+	port := helper.GetConfiguration().Port
 
 	router := gin.New()
 	router.Use(gin.Logger())
 	routes.UserRoutes(router)
 
 	router.Use(middleware.Authentication())
-
-	// API-2
-	router.GET("/api-1", func(c *gin.Context) {
-
-		c.JSON(200, gin.H{"success": "Access granted for api-1"})
-
-	})
-
-	// API-1
-	router.GET("/api-2", func(c *gin.Context) {
-		c.JSON(200, gin.H{"success": "Access granted for api-2"})
-	})
 
 	router.Run(":" + port)
 }
