9dc4189f1e3570ee90112294eb14b3287ec2f263
Eran Amarante <eran.amarante31@gmail.com>
2021-07-12 16:06:09 +0800

    update auth middleware

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -4,12 +4,10 @@ import (
 	"fmt"
 	"net/http"
 
-	helper "github.com/eranamarante/go-expense-tracker-api/helpers"
-
+	"github.com/eranamarante/go-expense-tracker-api/helper"
 	"github.com/gin-gonic/gin"
 )
 
-// Authz validates token and authorizes users
 func Authentication() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		clientToken := c.Request.Header.Get("token")
@@ -27,11 +25,10 @@ func Authentication() gin.HandlerFunc {
 		}
 
 		c.Set("email", claims.Email)
-		c.Set("first_name", claims.First_name)
-		c.Set("last_name", claims.Last_name)
+		c.Set("first_name", claims.FirstName)
+		c.Set("last_name", claims.LastName)
 		c.Set("uid", claims.Uid)
 
 		c.Next()
-
 	}
 }
